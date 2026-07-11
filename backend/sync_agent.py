b1a6b0596c3c6d13ecc8cd278741fa5128ceb56e
VisualVoyageStudios <visualvoyagerbsn@gmail.com>
2026-07-11 10:48:59 +0200

    Update sync_agent.py

--- backend/sync_agent.py
+++ backend/sync_agent.py
@@ -11,7 +11,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://voyageranalytics.netlify.app",
-                    "https://visualvoyagestudios.github.io/vanalytics"
+                   "https://visualvoyagestudio.github.io",
+                   "https://visualvoyagestudios.github.io/vanalytics",
                    "http://127.0.0.1:5500",
                    "http://localhost:5500"],
     allow_methods=["*"],
