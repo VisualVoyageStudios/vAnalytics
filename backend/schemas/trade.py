47d73e6d64c83bec8098404060fc59d764be7b7e
VisualVoyageStudios <visualvoyagerbsn@gmail.com>
2026-07-12 18:31:37 +0200

    Update trade.py

--- backend/schemas/trade.py
+++ backend/schemas/trade.py
@@ -1,18 +1,23 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class TradeCreate(BaseModel):
 
-    account_id:str
+    account_id: str
 
-    symbol:str
+    symbol: str
 
-    order_type:str
+    order_type: str
 
-    lot_size:float
+    lot_size: float
 
-    open_price:float
+    open_price: float
 
-    close_price:float
+    close_price: float
 
-    profit:float
+    profit: float
+
+    stop_loss: Optional[float] = None
+
+    take_profit: Optional[float] = None
